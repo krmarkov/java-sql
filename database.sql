ace1f84dd52ad40261698b24d0e989df7c55e419
kristian <kristiian.2004@gmail.com>
2023-03-27 15:31:03 +0300

    loginservlet

---
+++ database.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE website;
+USE website;
+CREATE TABLE users (id int, username varchar(255), password varchar(255));
+insert into users values (1, "admin", "admin"), (2, "momchil70k", "azsamgei");
