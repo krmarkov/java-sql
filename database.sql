65ad9253d5173ebeb564a4f6146ae6202000a756
kristian <kristiian.2004@gmail.com>
2023-04-26 13:07:48 +0300

    login and register

--- database.sql
+++ database.sql
@@ -2,3 +2,6 @@ drop database if exists website;
 create database website;
 use website;
 create table users(username varchar(255), password varchar(255), email varchar(255));
+create table products(name varchar(255), price float, picture varchar(255));
+insert into products values ("Бяла тениска", 19.99, "t-shirt-white.jpg"), ("Черна тениска", 19.99, "t-shirt-black.jpg"), ("Сива тениска", 19.99, "t-shirt-gray.jpg");
+insert into users values ("admin", "admin", "admin@site.com"), ("momchil", "1234", "umil2@abv.bg");
--- src/java/LoginServlet.java
+++ src/java/LoginServlet.java
@@ -5,17 +5,48 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet(urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+        throws ServletException, IOException {
+            try {
                 String username = request.getParameter("username");
                 String password = request.getParameter("password");
-                if (username.equals("admin") && password.equals("admin")) {
+
+                Class.forName("org.mariadb.jdbc.Driver");
+                Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/website");
+                Statement stmt = conn.createStatement();
+                
+                String selectUser = String.format("select count(*) from users where username = \"%s\"", username);
+                ResultSet rs1 = stmt.executeQuery(selectUser);
+                rs1.next();
+                
+                String selectPassword = String.format("select password from users where username = \"%s\"", username);
+                ResultSet rs2 = stmt.executeQuery(selectPassword);
+                rs2.next();
+                
+                
+                if (rs1.getString(1).equals("1") && rs2.getString(1).equals(password)) {
                     response.getWriter().println("Login successful");
                 }
-                else response.getWriter().println("Login failed");
+                else {
+                    response.getWriter().println("Login failed");
+                }
+            } 
+            catch (ClassNotFoundException ex) {
+                Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+            catch (SQLException ex) {
+                Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
+        }
     }
